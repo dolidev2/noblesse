eb4b1dc8e40076f5e2f6fc9e2164bffdf14dfe1f
dolidev2 <dolidev2.0@gmail.com>
2024-01-14 12:17:36 +0000

    update config cicd

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -6,15 +6,6 @@ echo "Deployment started ..."
 # Pull the latest version of the app
 cd /var/www/html/noblesse
 git pull
-# allow composer for root
-export COMPOSER_ALLOW_SUPERUSER=1;
-
-# Install composer dependencies
-composer install --no-dev --optimize-autoloader --no-interaction
-# composer update --lock
-
-# Install npm dependencies
-npm install
 
 echo "New changes copied to server !"
 
