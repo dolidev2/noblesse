2a26886233c6ada7c0980e441086aaa65a879671
dolidev2 <dolidev2.0@gmail.com>
2024-07-19 08:54:38 +0000

    caisse display view for fix error

--- view/page/caisse.php
+++ view/page/caisse.php
@@ -180,7 +180,9 @@
                                 </tr>
                                 </thead>
                                 <tbody>
-                                <?php $versement = Versement::read($_SESSION['agence']); $i=1;
+                                <?php
+                                    $versement = Versement::read($_SESSION['agence']); $i=1;
+
                                 ?>
                                 <?php foreach ($versement as $vs) : ?>
                                     <tr class="odd gradeX">
@@ -190,7 +192,44 @@
                                         <td><?=$vs->banque; ?></td>
                                         <td><?=$vs->mode; ?></td>
                                         <td><?=date('d/m/Y',strtotime($vs->date)); ?></td>
-
+                                        <td class="center">
+                                            <?php if ($_SESSION['fonction'] == 'administrateur') {  ?>
+                                            <a title="Modifier" class="btn btn-primary" href="index.php?page=de_versement&id_versement=<?= $vs->id_ver ?>">
+                                                <span class="fa fa-pencil"></span>
+                                            </a>
+                                                <button title="Supprimer" type="button" class="btn btn-danger" data-toggle="modal" data-target="<?='#modal_ver'.$i?>">
+                                                    <span class="fa fa-trash"></span>
+                                                </button>
+                                            <?php } ?>
+                                            <!-- Modal -->
+                                            <div class="modal fade" id="<?='modal_ver'.$i;?>" tabindex="-1" role="dialog" aria-labelledby="<?='#modal_ver'.$i;?>" aria-hidden="true">
+                                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title" id="<?='#modal_ver'.$i;?>">
+                                                            </h5>
+                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                <span aria-hidden="true">&times;</span>
+                                                            </button>
+                                                        </div>
+                                                        <form method="post" action="../control/del_versement.php">
+                                                            <div class="modal-body">
+                                                                <input type="hidden" name="id_ver" value="<?=$vs->id_ver?>">
+                                                                <button class="btn btn-danger">
+                                                                    <h3>
+                                                                        Voulez vous vraiment supprimer cette Transaction ?
+                                                                    </h3>
+                                                                </button>
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">NON</button>
+                                                                <button type="submit" name="submit" class="btn btn-primary">OUI</button>
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
                                     </tr>
                                     <?php $i++; ?>
                                 <?php endforeach; ?>
